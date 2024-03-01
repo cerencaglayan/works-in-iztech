2e96f55bd4c9c59b19fd5c591745d82544a518fb
cerencaglayan <cer79cag@gmail.com>
2024-03-01 14:09:02 +0300

    updated

--- Bash Scripting/Arrays.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#array declaration:
-my_array=(elma muz "Fruit Basket" orange 5) #syntax önemli.
-new_array[2]=kayısı
-echo ${my_array[0]}
-echo ${new_array[0]}
-
-# ${#my_array[@]}  number of elements 
--- Bash Scripting/First.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#We must add line 1.
-# Comment line
-echo 'Hello, World!'
